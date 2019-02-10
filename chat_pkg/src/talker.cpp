b75948cc8fa18bf0024d08cf7713f618d29a3ac7
DragonOfVoid <47323076+DragonOfVoid@users.noreply.github.com>
2019-02-10 09:19:08 -0600

    Add files via upload

---
+++ chat_pkg/src/talker.cpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+using namespace ros;
+
+int main (int argc, char **argv)
+{
+	init(argc, argv, "talker");
+	
+	NodeHandle node;
+	Publisher chatter_pub = node.advertise<std_msgs::String>("chatter", 1000);
+	Rate loop_rate(10);
+
+	int count;
+	while (ok())
+	{
+		std_msgs::String msg;
+		
+		std::stringstream ss;
+		ss << "hello world " << count;
+		msg.data = ss.str();
+		ROS_INFO("%s", msg.data.c_str());
+		
+		chatter_pub.publish(msg);
+		
+		ros::spinOnce();
+		
+		loop_rate.sleep();
+		count++; 
+	}
+	return 0;
+}
